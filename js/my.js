cadff227a14274e6dc5a2a142d676841d08c2580
npv34 <phanvanluan611996@gmail.com>
2021-08-16 21:16:17 +0700

    cong 2 so

---
+++ js/my.js
@@ -0,0 +1,27 @@
+/*
+  let/var/const + ten_bien = gia_tri;
+  let vs var khac nhau o pham vi su dung:
+  + let thi chi su dung ben trong scop bao ben ngoai bien
+  + var duoc su dung toan bo function
+  + const -> hang so: k thay doi dc
+  - Kieu du lieu:
+  + number
+  + string
+  + array
+  + object
+  + null
+  + undefined
+  + boolean
+  - Toan tu:
+  + Toan hoc
+  + Logic
+  + Gan
+  + So sanh 
+  + Tang giam
+  + Noi chuoi
+
+*/
+let a = +prompt("Nhap so thu 1");
+let b = +prompt("Nhap so thu 2");
+c = a + b;
+alert(c);
